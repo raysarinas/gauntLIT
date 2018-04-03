3f52c767675842c6d13f14a6715da4bd34377be6
raysarinas <rsarinas@ualberta.ca>
2018-04-02 23:09:12 -0600

    😈🧚‍♀️🧚‍♂️🔫
    I PUT SPRITES and yes i put bowser all the donkey kong ones were ugly

--- characters.py
+++ characters.py
@@ -5,8 +5,6 @@ import pygame, sys, time, board, random
 from pygame.locals import *
 vec = pygame.math.Vector2
 
-ORANGE = (255,128,0)
-
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
@@ -24,7 +22,7 @@ class Player: # player class. could possible change this to have options to have
         self.surface = surface
         self.surface_size = self.surface.get_size()
         self.color = pygame.Color('red')
-        self.rect = Rect(10, self.surface_size[1] - 30, 20, 20)
+        self.rect = Rect(10, self.surface_size[1] - 40, 20, 20)
 
 
         self.vel = vec(0, 0)
@@ -32,13 +30,17 @@ class Player: # player class. could possible change this to have options to have
 
 
     def draw(self):
-        pygame.draw.rect(self.surface, self.color, self.rect)
+        #pygame.draw.rect(self.surface, self.color, self.rect)
+        sprite = pygame.sprite.Sprite()
+        sprite.image = pygame.image.load('mario.png').convert_alpha()
+        sprite.rect = sprite.image.get_rect()
+        self.surface.blit(sprite.image, self.rect)
 
     def move_vert(self, rect, direction):
         size = self.surface.get_size()
         if direction < 0 and rect.top > 10:
                 rect.y = rect.y + direction * 10
-        if direction > 0 and rect.bottom < size[1] - 10:
+        if direction > 0 and rect.bottom < size[1] - 20:
                 rect.y = rect.y + direction * 10
 
     def move_horiz(self, rect, direction):
@@ -62,7 +64,7 @@ class Fireball:
 
     def __init__(self, surface):
         self.surface = surface
-        self.radius = 10
+        self.radius = 5
         self.center = [20, 20]
         self.color = pygame.Color('yellow')
         self.randomspeed = random.randint(1, 5)
@@ -71,7 +73,11 @@ class Fireball:
         self.walls = None
 
     def draw(self):
-        pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+        #pygame.draw.circle(self.surface, self.color, self.center, self.radius)
+        sprite = pygame.sprite.Sprite()
+        sprite.image = pygame.image.load('fireball.png').convert_alpha()
+        sprite.rect = sprite.image.get_rect()
+        self.surface.blit(sprite.image, [self.center[0] - 5, self.center[1] - 5])
 
     def move(self, walllist): # add in more parameters/arguments when walls are drawn
         for coord in range(2):
@@ -89,20 +95,17 @@ class Fireball:
             for wall in walls:
                 pass # this should check collisions but not sure how to do that yet
 
-
-
-
-class Peach:
-    peachColor = pygame.Color('pink') # replace with sprite?
-
-    def __init__(self, surface):
-        self.surface = surface
-        self.surface_size = self.surface.get_size()
-        self.color = pygame.Color('pink')
-        self.rect = Rect(self.surface_size[0] - 30, 10, 20, 20)
-
-    def draw(self):
-        pygame.draw.rect(self.surface, self.color, self.rect)
+# class Peach:
+#     peachColor = pygame.Color('pink') # replace with sprite?
+#
+#     def __init__(self, surface):
+#         self.surface = surface
+#         self.surface_size = self.surface.get_size()
+#         self.color = pygame.Color('pink')
+#         self.rect = Rect(self.surface_size[0] - 30, 10, 20, 20)
+#
+#     def draw(self):
+#         pygame.draw.rect(self.surface, self.color, self.rect)
 
 class DonkeyKong:
     kongColor = pygame.Color('brown')
@@ -114,4 +117,27 @@ class DonkeyKong:
         self.color = DonkeyKong.kongColor
 
     def draw(self):
-        pygame.draw.circle(self.surface, self.color, self.center, self.radius, 0)
+        #pygame.draw.circle(self.surface, self.color, self.center, self.radius, 0)
+        sprite = pygame.sprite.Sprite()
+        sprite.image = pygame.image.load('bowser.png.gif').convert_alpha()
+        sprite.rect = sprite.image.get_rect()
+        self.surface.blit(sprite.image, [self.center[0] - 30, self.center[1] - 30])
+
+class PrincessPeach:
+    def __init__(self, surface):
+        self.surface = surface
+        self.surface_size = self.surface.get_size()
+        self.location = [self.surface_size[0] - 30, 10]
+
+    # def makeImage(self):
+    #     file = 'peach.png'
+    #     image = pygame.image.load(file)
+    #     return image
+
+    def draw(self):
+        sprite = pygame.sprite.Sprite()
+        sprite.image = pygame.image.load('peach.png').convert_alpha()
+        sprite.rect = sprite.image.get_rect()
+        self.surface.blit(sprite.image, self.location)
+        # image = self.makeImage()
+        # self.surface.blit(image, self.location)
