063e722359557a1da7dd6ea6ffb04c791a533595
Melisse Doroteo <gabbie_doroteo@yahoo.com>
2018-04-10 14:16:02 -0600

    🔫

--- main/pathfinding.py
+++ main/pathfinding.py
@@ -8,17 +8,22 @@ from graphtest import *
 # PLEASE IMPLEMENT THE PATHFINDING STUFF IN THE FINDPATH FUNCTION NOT THE
 # GETPATH FUNCTION!!!!!
 # ******************************
-def distance(self, e):
+# def distance(self, e):
+def distance(start, end):
     """
     Here e is a pair (u,v) of vertices. / technically just an edge
     Returns the manhattan distance between the two vertices u and v.
     """
-    lon1, lat1 = e[0][0], e[0][1]
-    lon2, lat2 = e[1][0], e[1][0]
 
-    # calculate the manhattan distance between two points(x1,y1) and (x2,y2)
-    e_dist = abs((lat2 - lat1)) + abs((lon2 - lon1))
-    return e_dist
+    playerx = start[0]
+    playery = start[1]
+
+    blockx = end[0]
+    blocky = end[1]
+
+    man_dist = abs((playerx - blockx)) + abs((playery - blocky))
+    # print(playerx)
+    return man_dist #, playerx
 
 def getpath(playercoordx, playercoordy, blockx, blocky):
 
@@ -66,3 +71,71 @@ def findpath(playercoordx, playercoordy, blockx, blocky, graph):
 
     print('player coordinates:', playercoordx, playercoordy)
     print('ghost goordinates: ', blockx, blocky)
+
+    # startcoord = (playercoordx, playercoordy)
+    # destination = (blockx, blocky)
+    # startDist = distance(point , startcoord)
+    # endDist = distance(point, destination)
+    #
+    # print(startDist)
+    # print(endDist)
+
+
+    playercoords= [playercoordx, playercoordy]
+    ghostcoords = [blockx, blocky]
+
+    # initialize minimum start/end and start/end vertex values
+    # minStart = float('inf')
+    # minEnd = float('inf')
+
+    minplayer = float('inf')
+    minghost = float('inf')
+
+    startV = None
+    endV = None
+
+    # find the nearest vertex to the start and destination/end points
+    # that have been inputted/requested
+
+
+    #vertlist = list(graph.get_vertices())#######
+    #edgelist = graph.get_edges()
+
+    #print(distance(playercoords, ghostcoords))
+
+    vertlist = list(graph.get_vertices())
+    mintemp = 0
+    ghosttemp = 0
+
+    
+
+    for vertex in vertlist:
+        mintemp = distance(vertex, playercoords)
+        if mintemp < minplayer:
+            minplayer = mintemp
+        ghosttemp = distance(vertex, ghostcoords)
+        if ghosttemp < minghost:
+            minghost = ghosttemp
+
+    currentvertex = minplayer
+
+    print(currentvertex)
+
+    # print(minplayer)
+    # print(minghost)
+
+
+    # for vertex in vertlist:
+    #     #for point in edgelist
+    # #for vertex, point in vertlist, edgelist:#####
+    #     startDist = distance(point, startcoord)
+    #     endDist = distance(point, destination)
+    #     if startDist <= minStart:
+    #         startV = vertex
+    #         minStart = startDist
+    #     if endDist <= minEnd:
+    #         endV = vertex
+    #         minEnd = endDist
+    #
+    # print(startV)
+    # print(endV)
